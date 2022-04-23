1d432ab5a8def8cd97aa6306639751d44512fb85
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-04-23 10:29:40 +0100

    Persisting data to localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import List from './List';
 import Alert from './Alert';
 import './App.css';
 
+const getLocalStorage = () => {
+  let list = localStorage.getItem('list');
+  if (list) {
+    return JSON.parse(localStorage.getItem('list'));
+  } else {
+    return [];
+  }
+};
 function App() {
   const [name, setName] = useState('');
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(getLocalStorage());
   const [isEditing, setIsEditing] = useState(false);
   const [editId, setEditID] = useState(null);
   const [alert, setAlert] = useState({
@@ -68,6 +76,10 @@ function App() {
     setEditID(id);
     setName(specificItem.title);
   };
+
+  useEffect(() => {
+    localStorage.setItem('list', JSON.stringify(list));
+  }, [list]);
 
   return (
     <section className='section-center'>
